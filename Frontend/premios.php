5e68a6ef456e9d6736c5c9752f33b4034e73d619
Dario <darioefalzone95@gmail.com>
2020-06-19 04:29:47 -0300

    TROFEOS.PHP - CODIGO CORREGIDo

--- Frontend/premios.php
+++ Frontend/premios.php
@@ -67,27 +67,30 @@
         </div>
     </section>
     <!--CATEGORIA END-->
-
-    <!--TROPHEY ROOM BEGIN-->
-
+    <!--TROPHEY ROOM START-->
     <section class="trophey-room">
         <div class="container">
             <div class="row">
+                <!--TITULO-->
                 <div class="col-md-12">
                     <h4>Trophy room</h4>
                 </div>
-                <div class="col-md-8">
+                <!--SINOPSIS-->
+                <div class="col-md-12">
                     <p>Hella pop-up flexitarian, semiotics migas humblebrag schlitz literally tofu deep v thundercats skateboard viral cornhole. Lomo knausgaard truffaut selfies flexitarian, tbh swag kickstarter gastropub mustache readymade artisan keffiyeh gochujang. </p>
                 </div>
+                <!--IMAGEN-->
                 <div class="col-md-12">
                     <div class="trophey-media">
                         <img src="images/vs_arena/trophey-media-img.jpg" alt="trophey-media-img">
                     </div>
                 </div>
+                <!--TEXTO CUERPO-->
                 <div class="col-md-8">
                     <p>Pabst irony tattooed, synth sriracha selvage pok pok. Wayfarers kinfolk sartorial, helvetica you probably haven't heard of them tumeric venmo deep v mixtape semiotics brunch. Next level paleo taxidermy, bespoke messenger bag leggings occupy food truck. <br>Hella pop-up flexitarian, semiotics migas humblebrag schlitz literally tofu deep v thundercats skateboard viral cornhole. Lomo knausgaard truffaut selfies flexitarian, tbh swag kickstarter gastropub mustache readymade artisan keffiyeh gochujang.</p>
                     <p>Fixie four dollar toast meggings, 8-bit letterpress schlitz kale chips vexillologist yr venmo blog kitsch hammock affogato. Tbh kombucha typewriter pug, cliche ramps try-hard. Salvia enamel pin quinoa twee edison bulb, affogato typewriter unicorn cray asymmetrical. Scenester bitters kinfolk, small batch green juice cliche flexitarian poutine fixie cornhole dreamcatcher. Mustache irony pickled schlitz wayfarers tattooed. Kale chips roof party activated charcoal, paleo kogi affogato coloring book direct trade. Blue bottle dreamcatcher cardigan, bicycle rights live-edge shoreditch echo park sartorial deep v heirloom narwhal mumblecore.</p>
                 </div>
+                <!--ASIDE INFO-->
                 <div class="col-md-4">
                     <ul class="done-list">
                         <li>Win a championship</li>
@@ -99,17 +102,17 @@
             </div>
         </div>
     </section>
-
     <!--TROPHEY ROOM END-->
-    <!--MAIN AWARD BOX BEGIN-->
+    <!--MAIN AWARD BOX START-->
     <div class="main-award-box trophey-awards">
-
         <div class="main-award-slider">
             <div id="main-award-slider" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner" role="listbox">
+                    <!--TROFY ROOM START-->
                     <div class="item active">
                         <div class="container">
                             <div class="row">
+                                <!--TROFEOS-->
                                 <div class="col-xs-4 text-center">
                                     <div class="award">
                                         <div class="image"><img class="img-responsive" src="images/common/am-trophey.png" alt="cup-image"></div>
@@ -117,6 +120,7 @@
                                         <div class="year">2016</div>
                                     </div>
                                 </div>
+                                <!--TROFEOS-->
                                 <div class="col-xs-4 text-center">
                                     <div class="award">
                                         <div class="image"><img class="img-responsive" src="images/common/am-trophey1.png" alt="cup-image"></div>
@@ -124,6 +128,7 @@
                                         <div class="year">2016</div>
                                     </div>
                                 </div>
+                                <!--TROFEOS-->
                                 <div class="col-xs-4 text-center">
                                     <div class="award">
                                         <div class="image"><img class="img-responsive" src="images/common/am-trophey2.png" alt="cup-image"></div>
@@ -134,6 +139,8 @@
                             </div>
                         </div>
                     </div>
+                    <!--TROFY ROOM END-->
+                    <!--TROFY ROOM START-->
                     <div class="item">
                         <div class="container">
                             <div class="row">
@@ -161,10 +168,9 @@
                             </div>
                         </div>
                     </div>
-
+                    <!--TROFY ROOM END-->
                 </div>
-
-                <!-- Controls -->
+                <!--CONTROLES-->
                 <a class="nav-arrow left-arrow" href="#main-award-slider" role="button" data-slide="prev">
                     <i class="fa fa-angle-left" aria-hidden="true"></i>
                     <span class="sr-only">Previous</span>
@@ -175,11 +181,8 @@
                 </a>
             </div>
         </div>
-
     </div>
-
     <!--MAIN AWARD BOX END-->
-    <!--STANDING TABLE WRAP END-->
     <!--START FOOTER-->
     <?php
     include('estructura/footer.php');
