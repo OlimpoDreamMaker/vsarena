2ec2b0b13ce75dfc4a2c9b678d5b5341c1d78334
Dario <darioefalzone95@gmail.com>
2020-06-19 01:10:04 -0300

    PROX_ENFRENT_CPA.PHP - CODIGO CORREGIDO

---
+++ Frontend/prox_enfrent_cpa.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<!--START ROBOTS-->
+	<?php
+	include('estructura/robots.php');
+
+	?>
+	<!--END ROBOTS-->
+	<!--START METAS GLOBALES-->
+	<?php
+	include('estructura/global_meta.php');
+
+	?>
+	<!--END METAS GLOBALES-->
+	<!--START FUENTES-->
+	<?php
+	include('estructura/global_fuentes.php');
+
+	?>
+	<!--END FUENTES-->
+	<!--START HOJAS-->
+	<?php
+	include('estructura/global_hojas.php');
+
+	?>
+	<!--END HOJAS-->
+	<!--START METAS LOCALES-->
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<!--END METAS LOCALES-->
+	<!--START TITULO-->
+	<title>COPA - Próximo Enfrentamiento</title>
+	<!--END TITULO-->
+</head>
+
+<body>
+	<!--PRELOADER START-->
+	<?php
+	include('estructura/preloader.php');
+
+	?>
+	<!--PRELOADER END-->
+	<!--NAV START-->
+	<?php
+	include('estructura/barra_navegacion.php');
+
+	?>
+	<!--NAV END-->
+	<!--CATEGORIA START-->
+	<section class="image-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="info">
+						<div class="wrap">
+							<ul class="breadcrumbs">
+								<li><a href="index.html">Inicio</a>/</li>
+								<li>Prox. Enfrentamiento</li>
+							</ul>
+							<h1>Prox. Enfrentamiento</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--CATEGORIA END-->
+	<!--MATCH PAGE TOP START-->
+	<div class="match-page-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<!--INFORMACION DE ENFRENTAMIENTO START-->
+					<div class="upcoming-match-info">
+						<!--TEAM-->
+						<div class="team">
+							<div class="avatar"><img src="images/vs_arena/team-logo1.png" alt="match-list-team-img"></div>
+							<div class="text">
+								Team 1
+								<div class="latest">
+									<div class="latest-title">Latest Results</div>
+									<span class="win">V</span>
+									<span class="win">V</span>
+									<span class="win">V</span>
+									<span class="win">V</span>
+									<span class="win">V</span>
+								</div>
+							</div>
+						</div>
+						<!--CONTADOR-->
+						<div class="counter">
+							<ul>
+								<li>
+									<div class="digit">6</div>
+								</li>
+								<li>
+									<div class="digit">20</div>
+								</li>
+								<li>
+									<div class="digit">37</div>
+								</li>
+							</ul>
+						</div>
+						<!--TEAM-->
+						<div class="team right">
+							<div class="text">
+								Team 2
+								<div class="latest">
+									<div class="latest-title">Latest Results</div>
+									<span class="win">V</span>
+									<span class="drawn">E</span>
+									<span class="win">V</span>
+									<span class="lose">P</span>
+									<span class="win">V</span>
+								</div>
+							</div>
+							<div class="avatar"><img src="images/vs_arena/team-logo2.png" alt="match-list-team-img"></div>
+						</div>
+					</div>
+					<!--INFORMACION DE ENFRENTAMIENTO END-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--MATCH PAGE TOP END-->
+	<!--LIVE MACTH STAT START-->
+	<section class="live-match-stat">
+		<div class="container">
+			<div class="row">
+				<!--INFORMACION-->
+				<div class="col-lg-6">
+					<h5>SOBRE EL CAMPEONATO</h5>
+					<div class="live-details">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum vero debitis voluptatibus dolores aspernatur, nulla, consequatur nesciunt adipisci, ipsa distinctio deserunt error minus fugit. Sequi et, suscipit! Tenetur, laboriosam facere?.</div>
+					<div class="statistics">
+						<h6>Horario del enfrentamiento</h6>
+						<p>Stamford Bridge Arena<br>7:30 PM, Saturday, 29 Sep 2016</p>
+					</div>
+					<div class="live-championship">
+						<h3>Campeonato</h3>
+						<div class="header">LCS CHAMPIONSHIP</div>
+						<div class="border">
+							<div class="row">
+								<div class="col-md-4 col-sm-5">
+									<span class="img-champ">
+										<a href="championship.html" title="The Greatest League">
+											<img src="images/vs_arena/championship-ico.jpg" alt="champ-img">
+										</a>
+									</span>
+								</div>
+								<div class="col-md-8 col-sm-7">
+									<div class="info">
+										<dl>
+											<dt>Iniciado:</dt>
+											<dd>16 may 2020</dd>
+											<dt>Region:</dt>
+											<dd>Europe</dd>
+											<dt>Ultimo Champion:</dt>
+											<dd>Team Name 1</dd>
+										</dl>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--ALINEACION-->
+				<div class="col-lg-6">
+					<h5>Alineación</h5>
+					<div class="match-lineup">
+						<div class="no-gutter">
+							<div class="col-sm-6">
+								<!--TEAM 01-->
+								<div class="head">
+									<div class="name">SKT T1</div>
+								</div>
+								<!--MIEMBRO-->
+								<div class="member">
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+									<div class="info">
+										<div class="wrap">
+											<a href="player.html" class="name">Pasveer (GK)</a>
+											<div class="card yellow" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<div class="card red" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+										</div>
+										<div class="country"><img src="images/common/group_icon1.png" alt="flag">Lorem</div>
+									</div>
+								</div>
+								<!--MIEMBRO-->
+								<div class="member">
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+									<div class="info">
+										<div class="wrap">
+											<a href="player.html" class="name">Pasveer (GK)</a>
+											<div class="card yellow" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<div class="card red" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+										</div>
+										<div class="country"><img src="images/common/group_icon1.png" alt="flag">Lorem</div>
+									</div>
+								</div>
+								<!--MIEMBRO-->
+								<div class="member">
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+									<div class="info">
+										<div class="wrap">
+											<a href="player.html" class="name">Pasveer (GK)</a>
+											<div class="goal" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+										</div>
+										<div class="country"><img src="images/common/group_icon1.png" alt="flag">Lorem</div>
+									</div>
+								</div>
+								<!--MIEMBRO-->
+								<div class="member">
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+									<div class="info">
+										<div class="wrap">
+											<a href="player.html" class="name">Pasveer (GK)</a>
+											<div class="card yellow" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+										</div>
+										<div class="country"><img src="images/common/group_icon1.png" alt="flag">Lorem</div>
+									</div>
+								</div>
+								<!--MIEMBRO-->
+								<div class="member">
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+									<div class="info">
+										<div class="wrap">
+											<a href="player.html" class="name">Pasveer (GK)</a>
+											<div class="card yellow" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<div class="card red" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+										</div>
+										<div class="country"><img src="images/common/group_icon1.png" alt="flag">Lorem</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<!--TEAM 02-->
+							<div class="head">
+								<div class="name right">Giants</div>
+							</div>
+							<div class="team-wrap right">
+								<!--MIEMBRO-->
+								<div class="member">
+									<div class="info">
+										<div class="wrap">
+											<div class="card yellow" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<div class="goal" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<a href="player.html" class="name">Pasveer (GK)</a>
+										</div>
+										<div class="country"><img src="images/common/group_icon.png" alt="ícono del grupo"> Lorem</div>
+									</div>
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+								</div>
+								<!--MIEMBRO-->
+								<div class="member">
+									<div class="info">
+										<div class="wrap">
+											<div class="card yellow" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<div class="goal" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<a href="player.html" class="name">Pasveer (GK)</a>
+										</div>
+										<div class="country"><img src="images/common/group_icon.png" alt="ícono del grupo"> Lorem</div>
+									</div>
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+								</div>
+								<!--MIEMBRO-->
+								<div class="member">
+									<div class="info">
+										<div class="wrap">
+											<div class="card yellow" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<div class="goal" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<a href="player.html" class="name">Pasveer (GK)</a>
+										</div>
+										<div class="country"><img src="images/common/group_icon.png" alt="ícono del grupo"> Lorem</div>
+									</div>
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+								</div>
+								<!--MIEMBRO-->
+								<div class="member">
+									<div class="info">
+										<div class="wrap">
+											<div class="card yellow" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<div class="goal" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<a href="player.html" class="name">Pasveer (GK)</a>
+										</div>
+										<div class="country"><img src="images/common/group_icon.png" alt="ícono del grupo"> Lorem</div>
+									</div>
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+								</div>
+								<!--MIEMBRO-->
+								<div class="member">
+									<div class="info">
+										<div class="wrap">
+											<div class="card yellow" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<div class="goal" data-toggle="tooltip" data-placement="top" title="" data-original-title="32’"></div>
+											<a href="player.html" class="name">Pasveer (GK)</a>
+										</div>
+										<div class="country"><img src="images/common/group_icon.png" alt="ícono del grupo"> Lorem</div>
+									</div>
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--ENTRENADORES-->
+				<h3>Coaches</h3>
+				<div class="match-lineup">
+					<div class="no-gutter">
+						<div class="col-sm-6">
+							<div class="team-wrap">
+								<!--MIEMBRO-->
+								<div class="member">
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+									<div class="info">
+										<div class="wrap">
+											<a href="player.html" class="name">Coach 1</a>
+										</div>
+										<div class="country"><img src="images/common/group_icon1.png" alt="flag">Lorem</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="team-wrap right">
+								<!--MIEMBRO-->
+								<div class="member">
+									<div class="info">
+										<div class="wrap">
+											<a href="player.html" class="name">Coach 2 </a>
+										</div>
+										<div class="country"><img src="images/common/group_icon.png" alt="ícono del grupo"> Lorem</div>
+									</div>
+									<a href="player.html" class="avatar"><img src="images/common/member-avatar.jpg" alt="member-avatar"></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--LIVE MACTH STAT END-->
+	<!--REDES SOCIALES START-->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="share-box">
+					<div class="item">
+						<div class="container-fluid">
+							<div class="row no-gutter">
+								<div class="col-md-4 col-sm-6 col-xs-12">
+									<div class="title">COMPARTE ESTE CAMPEONATO</div>
+								</div>
+								<div class="col-md-8 col-sm-6 col-xs-12 text-right">
+									<ul class="share-socials  wpb_animate_when_almost_visible wpb_fadeInRight fadeInRight wpb_start_animation animated">
+										<!-- Digg -->
+										<li class="digg">
+											<a href="http://www.digg.com/">
+												<i class="fa fa-digg" aria-hidden="true"></i>
+											</a>
+										</li>
+										<!-- Facebook -->
+										<li class="facebook">
+											<a href="http://www.facebook.com/">
+												<i class="fa fa-facebook-square" aria-hidden="true"></i>
+											</a>
+										</li>
+										<!-- Google+ -->
+										<li class="google">
+											<a href="https://plus.google.com/">
+												<i class="fa fa-google-plus-square" aria-hidden="true"></i>
+											</a>
+										</li>
+										<!-- LinkedIn -->
+										<li class="linkedin">
+											<a href="http://www.linkedin.com/">
+												<i class="fa fa-linkedin-square" aria-hidden="true"></i>
+											</a>
+										</li>
+										<!-- Pinterest -->
+										<li class="pinterest">
+											<a href="http://assets.pinterest.com/">
+												<i class="fa fa-pinterest-square" aria-hidden="true"></i>
+											</a>
+										</li>
+										<!-- Reddit -->
+										<li class="reddit">
+											<a href="http://reddit.com/">
+												<i class="fa fa-reddit-square" aria-hidden="true"></i>
+											</a>
+										</li>
+										<!-- StumbleUpon-->
+										<li class="StumbleUpon">
+											<a href="http://www.stumbleupon.com/">
+												<i class="fa fa-stumbleupon-circle" aria-hidden="true"></i>
+											</a>
+										</li>
+										<!-- Tumblr-->
+										<li class="tumbler">
+											<a href="http://www.tumblr.com/">
+												<i class="fa fa-tumblr-square" aria-hidden="true"></i>
+											</a>
+										</li>
+										<!-- Twitter -->
+										<li class="twitter">
+											<a href="https://twitter.com/">
+												<i class="fa fa-twitter-square" aria-hidden="true"></i>
+											</a>
+										</li>
+										<!-- VK -->
+										<li class="vk">
+											<a href="http://vkontakte.ru/">
+												<i class="fa fa-vk" aria-hidden="true"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--REDES SOCIALES end-->
+	<!--START FOOTER-->
+	<?php
+	include('estructura/footer.php');
+
+	?>
+	<!--END FOOTER-->
+	<!--START SCRIPTS-->
+	<?php
+	include('estructura/scripts.php');
+
+	?>
+	<!--END SCRIPTS-->
+</body>
+
+</html>
