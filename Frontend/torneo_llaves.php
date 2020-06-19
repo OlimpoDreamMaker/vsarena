ba8251ee2411d00de73aeac506e80d50ae414d40
Dario <darioefalzone95@gmail.com>
2020-06-19 03:26:43 -0300

    TORNEO_LLAVES.PHP - CODIGO CORREGIDO

--- Frontend/torneo_llaves.php
+++ Frontend/torneo_llaves.php
@@ -48,9 +48,6 @@
 
     ?>
     <!--NAV END-->
-
-
-
     <!--CATEGORIA START-->
     <section class="image-header">
         <div class="container">
@@ -70,11 +67,11 @@
         </div>
     </section>
     <!--CATEGORIA END-->
-
     <!--STANDING CUP BEGIN-->
     <section class="standing-cup">
         <div class="container">
             <div class="row">
+                <!--TITULO-->
                 <div class="col-md-12">
                     <h4>Copa CLS</h4>
                     <ul class="tab-filters">
@@ -84,6 +81,7 @@
                         <li><a href="#final">Final</a></li>
                     </ul>
                 </div>
+                <!--LLAVES-->
                 <div class="col-md-12 overflow-scroll">
                     <div class="tab-content">
                         <div class="cup-body tab-pane active" id="qualification">
@@ -100,71 +98,18 @@
         </div>
     </section>
     <!--STANDING CUP END-->
+    <!--START FOOTER-->
+    <?php
+    include('estructura/footer.php');
 
+    ?>
+    <!--END FOOTER-->
+    <!--START SCRIPTS-->
+    <?php
+    include('estructura/scripts.php');
 
-    <script type="text/javascript" src="js/library/jquery.js"></script>
-    <script type="text/javascript" src="js/library/jquery-ui.js"></script>
-    <script type="text/javascript" src="js/library/bootstrap.js"></script>
-    <script type="text/javascript" src="js/library/jquery.sticky.js"></script>
-    <script type="text/javascript" src="js/library/jquery.jcarousel.js"></script>
-    <script type="text/javascript" src="js/library/jcarousel.connected-carousels.js"></script>
-    <script type="text/javascript" src="js/library/owl.carousel.js"></script>
-    <script type="text/javascript" src="js/library/progressbar.js"></script>
-    <script type="text/javascript" src="js/library/jquery.bracket.min.js"></script>
-    <script type="text/javascript" src="js/library/chartist.js"></script>
-    <script type="text/javascript" src="js/library/Chart.js"></script>
-    <script type="text/javascript" src="js/library/fancySelect.js"></script>
-    <script type="text/javascript" src="js/library/isotope.pkgd.js"></script>
-    <script type="text/javascript" src="js/library/imagesloaded.pkgd.js"></script>
-
-    <script type="text/javascript" src="js/jquery.team-coundown.js"></script>
-    <script type="text/javascript" src="js/matches-slider.js"></script>
-    <script type="text/javascript" src="js/header.js"></script>
-    <script type="text/javascript" src="js/matches_broadcast_listing.js"></script>
-    <script type="text/javascript" src="js/news-line.js"></script>
-    <script type="text/javascript" src="js/match_galery.js"></script>
-    <script type="text/javascript" src="js/main-club-gallery.js"></script>
-    <script type="text/javascript" src="js/product-slider.js"></script>
-    <script type="text/javascript" src="js/circle-bar.js"></script>
-    <script type="text/javascript" src="js/standings.js"></script>
-    <script type="text/javascript" src="js/shop-price-filter.js"></script>
-    <script type="text/javascript" src="js/timeseries.js"></script>
-    <script type="text/javascript" src="js/radar.js"></script>
-    <script type="text/javascript" src="js/slider.js"></script>
-    <script type="text/javascript" src="js/preloader.js"></script>
-    <script type="text/javascript" src="js/diagram.js"></script>
-    <script type="text/javascript" src="js/bi-polar-diagram.js"></script>
-    <script type="text/javascript" src="js/label-placement-diagram.js"></script>
-    <script type="text/javascript" src="js/donut-chart.js"></script>
-    <script type="text/javascript" src="js/animate-donut.js"></script>
-    <script type="text/javascript" src="js/advanced-smil.js"></script>
-    <script type="text/javascript" src="js/svg-path.js"></script>
-    <script type="text/javascript" src="js/pick-circle.js"></script>
-    <script type="text/javascript" src="js/horizontal-bar.js"></script>
-    <script type="text/javascript" src="js/gauge-chart.js"></script>
-    <script type="text/javascript" src="js/stacked-bar.js"></script>
-
-    <script type="text/javascript" src="js/library/chartist-plugin-legend.js"></script>
-    <script type="text/javascript" src="js/library/chartist-plugin-threshold.js"></script>
-    <script type="text/javascript" src="js/library/chartist-plugin-pointlabels.js"></script>
-
-    <script type="text/javascript" src="js/treshold.js"></script>
-    <script type="text/javascript" src="js/visible.js"></script>
-    <script type="text/javascript" src="js/anchor.js"></script>
-    <script type="text/javascript" src="js/landing_carousel.js"></script>
-    <script type="text/javascript" src="js/landing_sport_standings.js"></script>
-    <script type="text/javascript" src="js/twitterslider.js"></script>
-    <script type="text/javascript" src="js/champions.js"></script>
-    <script type="text/javascript" src="js/landing_mainnews_slider.js"></script>
-    <script type="text/javascript" src="js/carousel.js"></script>
-    <script type="text/javascript" src="js/video_slider.js"></script>
-    <script type="text/javascript" src="js/footer_slides.js"></script>
-    <script type="text/javascript" src="js/player_test.js"></script>
-
-    <script type="text/javascript" src="js/main.js"></script>
-
-
-
+    ?>
+    <!--END SCRIPTS-->
 </body>
 
 </html>
