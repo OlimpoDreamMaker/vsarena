ab0c088b6f7231ac8e93d5be20b4e00a5542c54b
OlimpoDreamMaker <56230907+OlimpoDreamMaker@users.noreply.github.com>
2020-06-20 16:55:47 -0300

    Lista para la web ETAPA03

--- BackEnd/config.php
+++ BackEnd/config.php
@@ -5,11 +5,11 @@ $urlBE = "https://vsarena.net/BackEnd";
 $urlFE = "https://vsarena.net/FrontEnd";
 $amigable = "https://vsarena.net";
 $imagenes = "https://vsarena.net/imagenes";
-$urlDB = "http://localhost/VSARENA/Dashboard";//Mientras use en local usar esta direccion
-$urlFE = "http://localhost/VSARENA/FrontEnd";//Mientras use en local usar esta direccion
-$urlBE = "http://localhost/VSARENA/BackEnd";//Mientras use en local usar esta direccion
-$amigable = "http://localhost/VSARENA";//Mientras use en local usar esta direccion
-$imagenes = "http://localhost/VSARENA/imagenes";//Mientras use en local usar esta direccion
+//$urlDB = "http://localhost/VSARENA/Dashboard";//Mientras use en local usar esta direccion
+//$urlFE = "http://localhost/VSARENA/FrontEnd";//Mientras use en local usar esta direccion
+//$urlBE = "http://localhost/VSARENA/BackEnd";//Mientras use en local usar esta direccion
+//$amigable = "http://localhost/VSARENA";//Mientras use en local usar esta direccion
+//$imagenes = "http://localhost/VSARENA/imagenes";//Mientras use en local usar esta direccion
 //Redes
 $facebook = "https://www.facebook.com/TorneosVSArena";
 $twitch = "https://www.twitch.tv/vsarenatv";
@@ -23,10 +23,10 @@ function conectar(){
   $s = 'localhost'; //Servidor
   $u = 'u932243340_154152'; //Root
   $p = '123456789';//Password
-  $d = 'root';//Base de Datos
-  $u = 'root'; //Root
-  $p = '';//Password
-  $d = 'vsarena';//Base de Datos
+  //$d = 'root';//Base de Datos
+  //$u = 'root'; //Root
+  //$p = '';//Password
+  $d = 'u932243340_vsarena';//Base de Datos
   $con = mysqli_connect($s, $u, $p, $d);
   return $con;
 }
