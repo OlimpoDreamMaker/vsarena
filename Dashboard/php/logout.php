c962d9b0f318b63db331b947f34d6d38ea163b4d
Jose Ignacio Monzon <josemonzon99@gmail.com>
2020-07-10 19:02:23 -0300

    este si lo fixea

--- Dashboard/php/logout.php
+++ Dashboard/php/logout.php
@@ -1,5 +1,5 @@
 <?php
-  require_once("./config.php");
+  require_once("../../BackEnd/config.php");
   session_start();
   session_destroy();
   header("Location: $amigable/");//Enviar a pagina principal
