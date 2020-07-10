98b4c98b75effdf9d0630b97f3671a578e488564
Dario <darioefalzone95@gmail.com>
2020-07-10 20:22:23 -0300

    Agregué el login y registro de ejemplo

--- FrontEnd/estructura/global_hojas.php
+++ FrontEnd/estructura/global_hojas.php
@@ -5,5 +5,5 @@
 
         <link rel='icon' href='$urlFE/favicon.ico' type='image/x-icon' />
         <link href='$urlFE/css/style.min.css' rel='stylesheet' type='text/css' />
-        
+    
     <!--END HOJAS-->");
